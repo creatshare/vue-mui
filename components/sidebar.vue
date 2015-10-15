95f7877cf6532f611635b99c5e82d8707f09cfe6
mennghao <296469203@qq.com>
2015-10-16 00:01:57 +0800

    update:As the aside opened, fix body position

--- components/sidebar.vue
+++ components/sidebar.vue
@@ -21,17 +21,77 @@
 
 <script>
 module.exports = {
+	data : function(){
+		return {
+			name : 'aside-fix',
+			top : 0
+		}
+	},
 	props : {
 		show : {
 			type : Boolean,
 			default : false
 		}
 	},
+	watch : {
+		'show' : function(val){
+			this.fix(val);
+		}
+	},
 	methods : {
+		/**
+		 * [close this component]
+		 * @param  {[type]} e [description]
+		 * @return {[type]}   [description]
+		 */
 		close : function(e){
 			var that = this;
 			if (e.target.className === 't-aside-dimmer') {
 				that.show = false;
+			}
+		},
+		/**
+		 * [As the aside opened, fix body position]
+		 * @return {[type]} [description]
+		 */
+		fix : function(data){
+			var that = this,
+				_html = document.documentElement,
+				_body = document.body;
+
+			if (data) {
+
+				that.top = _body.scrollTop;
+
+				_html.style.marginTop = '-' + that.top + 'px';
+
+				that.setBodyStyle(_body, _html.getBoundingClientRect().width, window.screen.height);
+			} else {
+
+				//wait transiton end
+				setTimeout(function(){
+					_html.style.marginTop = '';
+					that.setBodyStyle.call(that, _body, '','',that.top);
+				}, 300);
+				
+			}
+			
+
+		},
+		/**
+		 * [setBodyStyle description]
+		 * @param {[type]} w [width]
+		 * @param {[type]} h [height]
+		 * @param {[type]} t [scrollTop]
+		 */
+		setBodyStyle : function(b, w, h, t){
+			b.style.width = w + 'px';
+			b.style.height = h + 'px';
+			if (t) {
+				b.classList.remove(this.name);
+				b.scrollTop = t;
+			} else {
+				b.classList.add(this.name);
 			}
 		}
 	}
