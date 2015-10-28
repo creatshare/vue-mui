6e95e719709f4d2006e9fd9700c2efb7fb582bf9
mennghao <296469203@qq.com>
2015-10-28 23:21:01 +0800

    add:new component menulist

---
+++ components/menu.vue
@@ -0,0 +1,33 @@
+<template>
+<ul class="t-menu">
+	<li v-repeat="its:list">
+		<a v-text="its.name" v-on="click:handle($event,its.link)"></a>
+		<ul class="t-menu__child" v-if="its.child">
+			<li v-repeat="items:its.child">
+				<a v-attr="href:items.link" v-text="items.name"></a>
+			</li>
+		</ul>
+	</li>
+</ul>
+</template>
+<script>
+module.exports = {
+	props : {
+		list : {
+			type : Array
+		}
+	},
+	methods : {
+
+		handle : function(e, link){
+			if (link) {
+				window.location.href = link;
+			} else {
+				var next = e.target.nextElementSibling;
+
+				next.style.display = next.style.display === 'block' ? 'none' : 'block';
+			}
+		}
+	}
+}
+</script>
---
+++ example/test/menuTest.vue
@@ -0,0 +1,85 @@
+<template>
+<div class="info__bd" id="m-menu-menu">
+	<h2>MenuList</h2>
+	<div class="info__demo">
+		
+		<div class="info__demo--show">
+			<menulist list="{{list}}"></menulist>		
+		</div>
+		
+		<pre>
+			<code class="language-markup">
+				<script type="language-mark-up">
+<menulist list="{{list}}"></menulist>
+				</script>
+			</code>
+		</pre>
+		<pre>
+			<code class="language-javascript">
+var Menu = require('components/menu.vue');
+
+new Vue({
+	data : function(){
+		return {
+			list : [{
+				name : 'Hello',
+				link : 'http://mui.yaobieting.com'
+			},{
+				name : 'World',
+				child : [{
+					name : 'test',
+					link : 'http://mui.yaobieting.com'
+				},{
+					name : 'test2',
+					link : 'http://mui.yaobieting.com'
+				}]
+			},{
+				name : 'Test',
+				link : 'http://mui.yaobieting.com'
+			}]
+		}
+	},
+	components : {
+		'menulist' : Menu
+	}
+	
+})
+			</code>
+		</pre>
+	</div>
+
+</div>
+
+
+</template>
+
+<script>
+var Menu = require('../../components/menu.vue');
+
+module.exports = {
+	data : function(){
+		return {
+			list : [{
+				name : 'Hello',
+				link : 'http://mui.yaobieting.com'
+			},{
+				name : 'World',
+				child : [{
+					name : 'test',
+					link : 'http://mui.yaobieting.com'
+				},{
+					name : 'test2',
+					link : 'http://mui.yaobieting.com'
+				}]
+			},{
+				name : 'Test',
+				link : 'http://mui.yaobieting.com'
+			}]
+		}
+	},
+	components : {
+		'menulist' : Menu
+	}
+	
+}
+</script>
